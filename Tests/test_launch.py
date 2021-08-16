c3dfbdb1930e0ea86ec70363484684a83b68c9ee
Tyler <tylerjp31@gmail.com>
2021-08-15 19:00:51 -0500

    - Added a test to print the current url - Added a test to verify the page title is in the page source

--- Tests/test_launch.py
+++ Tests/test_launch.py
@@ -8,17 +8,35 @@ import unittest
 class LaunchWebsite(WebDriverSetup):
 
     def test_Home_Page(self):
-        driver = self.driver
+        # driver = self.driver
 
         web_page_title = "Domain Names, Websites, Hosting & Online Marketing Tools - GoDaddy"
 
         # Checks if the value grabbed by driver.title matches web_page_title
         try:
-            if driver.title == web_page_title:
-                print("Page loaded successfully")
-                self.assertEqual(driver.title, web_page_title)
+            if self.driver.title == web_page_title:
+                print(self.driver.title + "Page loaded successfully")
+                self.assertEqual(self.driver.title, web_page_title)
         except Exception as error:
-            print(error+"Page failed to load")
+            print(error + "Page failed to load")
+
+    def test_current_url(self):
+        url = self.driver.current_url
+        print(url)
+
+    def test_page_source(self):
+        # pagesource = self.driver.page_source
+        with open('page_source.html', 'r', encoding='utf-8') as f:
+            if self.driver.title in f.read():
+                print("Page title is in page source")
+
+        # self.assertIn(self.driver.title, "page_source.html")
+        # fw = open("page_source.html", "w", encoding="utf-8")
+        # fw.write(pagesource)
+        # fw.close()
+        # fr = open("page_source.html", "r", encoding="utf-8")
+        # self.assertIn(self.driver.title, fr)
+        # fr.close()
 
 
 if __name__ == '__main__':
